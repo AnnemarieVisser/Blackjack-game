eddfc9e993189e5daf149a265749dd364053b84e
Annemarie Visser <annemarie@google64.nl>
2021-05-31 20:42:30 +0200

    Added some styling and event listeners

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-let firstCard = getRandomNumber(2, 11);
-let secondCard = getRandomNumber(2, 11);
-let hasBlackJack = false;
-let playerIsAlive = true;
-let message = "";
+function resetGame() {
+    let firstCard;
+    let secondCard;
+    let sumOfFirstAndSecondCard;
+    let hasBlackJack = false;
+    let playerIsAlive = true;
+    displayMessage.textContent = "Want to play a round?";
+    displayCards.textContent = "Cards: ";
+    displaySum.textContent = "Sum: ";
+}
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-let sumOfFirstAndSecondCard = firstCard + secondCard;
-
-if (sumOfFirstAndSecondCard < 21) {
-    message = "Do you want to draw a new card?"
-} else if (sumOfFirstAndSecondCard === 21) {
-    message = "Wohoo! You've got Blackjack!"
-    hasBlackJack = true;
-} else {
-    message = "You're out of the game!"
-    playerIsAlive = false;
+function message(sumOfCards) {
+    if (sumOfCards < 21) {
+        return "Do you want to draw a new card?"
+    } else if (sumOfCards === 21) {
+        hasBlackJack = true;
+        return "You've got Blackjack!"
+    } else {
+        playerIsAlive = false;
+        return "You're out of the game!"
+    }
 }
 
-console.log(hasBlackJack);
-console.log(playerIsAlive);
-console.log(message);
+function startTheGame() {
+    let firstCard = getRandomNumber(2, 11);
+    let secondCard = getRandomNumber(2, 11);
+    let sumOfFirstAndSecondCard = firstCard + secondCard;
+    console.log(sumOfFirstAndSecondCard);
+    console.log(message(sumOfFirstAndSecondCard));
+    displayMessage.textContent = message(sumOfFirstAndSecondCard);
+    displaySum.textContent = `Sum: ${sumOfFirstAndSecondCard}`;
+}
+
+const displayMessage = document.getElementById("message-el");
+const displaySum = document.getElementById("message-sum");
+const displayCards = document.getElementById("message-cards");
+
+const startbutton = document.getElementById("startbutton");
+startbutton.addEventListener("click", startTheGame);
+
+const resetbutton = document.getElementById("resetbutton");
+resetbutton.addEventListener("click", resetGame);
